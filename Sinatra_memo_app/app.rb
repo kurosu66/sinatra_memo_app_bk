54681155c94b352b2dcd5004ea95b74ed622d2c2
kurosu66 <kurosu.66@gmail.com>
2022-10-16 22:30:26 +0900

    メモアプリ実装

--- Sinatra_memo_app/app.rb
+++ Sinatra_memo_app/app.rb
@@ -1,53 +1,93 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
+require 'json'
 
-
-ActiveRecord::Base.establish_connection(
-  "adapter" => "sqlite3",
-  "database" => "./bbs.db"
-)
-
-class Comment < ActiveRecord::Base
+helpers do
+  def sanitize(text)
+    Rack::Utils.escape_html(text)
+  end
 end
 
-get '/home' do
-  @comments = Comment.order("id desc").all
-  erb :home
+memos = File.open('memos.json') do |memo|
+  JSON.parse(memo.read)
+end
+
+get '/memos' do
+  @memos = memos
+  erb :memos
 end
 
 get '/new' do
   erb :new
 end
 
-post '/new' do
-  s = Comment.new
-  s.title = params[:title]
-  s.body = params[:body]
-  s.save
+post '/memos' do
+  next_memo_id = 1
+  unless memos.nil?
+    memos['memos'].each do |memo|
+      next_memo_id = memo.keys[0].to_i + 1
+    end
+  end
 
-  erb :new
+  File.open('memos.json', 'w') do |file|
+    memos['memos'] << { next_memo_id.to_s => { 'title' => params[:title], 'content' => params[:content] } }
+    JSON.dump(memos, file)
+  end
+
+  redirect '/memos'
 end
 
-get '/detail/:id' do
-  @memo_detail = Comment.find(params[:id])
+get '/memos/:id' do
+  @memo_id = params[:id].to_i
+  memos['memos'].each do |memo|
+    if memo[@memo_id.to_s]
+      @memo_title = memo[@memo_id.to_s]['title']
+      @memo_content = memo[@memo_id.to_s]['content']
+    end
+  end
+
   erb :detail
 end
 
-get '/edit/:id' do
-  @memo_detail = Comment.find(params[:id]) 
+get '/memos/:id/edit' do
+  @memo_id = params[:id].to_i
+  memos['memos'].each do |memo|
+    if memo[@memo_id.to_s]
+      @memo_title = memo[@memo_id.to_s]['title']
+      @memo_content = memo[@memo_id.to_s]['content']
+    end
+  end
 
   erb :edit
 end
 
-delete '/detail/delete/:id' do
-  s = Comment.find(params[:id])
-  s.destroy
-  redirect '/home'
+patch '/memos/:id/update' do
+  @memo_id = params[:id].to_i
+  File.open('memos.json', 'w') do |file|
+    memos['memos'].each do |memo|
+      if memo[@memo_id.to_s]
+        memo[@memo_id.to_s]['title'] = params[:title]
+        memo[@memo_id.to_s]['content'] = params[:content]
+      end
+    end
+    JSON.dump(memos, file)
+  end
+
+  redirect '/memos'
 end
 
-patch '/patch/:id' do
-  @memo_detail = Comment.find(params[:id])
-  @memo_detail.update(title: params[:title], body: params[:body])
-  redirect '/home'
+delete '/memos/:id' do
+  @memo_id = params[:id].to_i
+  memos['memos'].delete_if do |memo|
+    memo[@memo_id.to_s]
+  end
+
+  File.open('memos.json', 'w') do |file|
+    JSON.dump(memos, file)
+  end
+
+  redirect '/memos'
 end
